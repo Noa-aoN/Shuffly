c7e577e3745938037cc7cc4e4a9fa20e4f0d7cfb
Noa-aoN <noa888aon@gmail.com>
2025-12-19 03:53:55 +0900

    add:メタタグ・OGP画像設定、update:ファビコン・アップルタッチアイコン画像更新

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem "bootsnap", require: false
 # ログイン認証
 gem "devise"
 
+# メタタグ・OGP設定
+gem "meta-tags"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_default_meta
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
@@ -19,5 +20,35 @@ class ApplicationController < ActionController::Base
   # option: サインイン後の遷移先
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || mypage_path
+  end
+
+  private
+
+  def set_default_meta
+    # gem 'meta-tags' を利用してデフォルトのメタタグを設定
+    set_meta_tags(
+      site: 'Shuffly',
+      title: 'Shuffly —グループ・順番・役割決めツール—',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Shufflyを使えばランダムなグループ分け・順番決め・役割決めが簡単にできます。',
+      keywords: 'グループ分け,順番決め,役割決め,ランダム,ツール,Shuffly',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: 'Shuffly',
+        title: 'Shuffly',
+        description: 'Shufflyを使えばランダムなグループ分け・順番決め・役割決めが簡単にできます。',
+        type: 'website',
+        url: request.original_url,
+        image: view_context.image_url('ogp.png'),
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        # site: '@',
+        image: view_context.image_url('ogp.png')
+      }
+    )
   end
 end
